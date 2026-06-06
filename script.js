949ca56a94d13c069335929b0873e89ae45dc928
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-06 20:26:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("YOGIEZ READY");
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const sidebar = document.getElementById("sidebar");
+
+menuBtn.addEventListener("click", () => {
+sidebar.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+sidebar.classList.remove("active");
+});
