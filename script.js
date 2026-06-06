62640bcb8352cad4188e38e3d065cebcf8d10d75
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-06 20:38:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ sidebar.classList.add("active");
 closeBtn.addEventListener("click", () => {
 sidebar.classList.remove("active");
 });
+
+const cartBtn = document.getElementById("cartBtn");
+const closeCart = document.getElementById("closeCart");
+const cartSidebar = document.getElementById("cartSidebar");
+
+if(cartBtn){
+
+cartBtn.addEventListener("click",()=>{
+
+cartSidebar.classList.add("active");
+
+});
+
+}
+
+if(closeCart){
+
+closeCart.addEventListener("click",()=>{
+
+cartSidebar.classList.remove("active");
+
+});
+
+}
